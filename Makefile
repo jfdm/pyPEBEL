c8465b86155a04378ddf61b66ec58f84ca7a2ba3
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2013-04-03 13:27:57 +0100

    Updated documentation to reflect recent changes in project.

--- Makefile
+++ Makefile
@@ -6,13 +6,18 @@
 SRC=pebel
 NAME=pebel
 
-.PHONY: usage pep8 apidocs clean
+.PHONY: usage pep8 apidocs clean pylint install
 
 usage: # Print Targets
 	@grep '^[^#[:space:]].*:' Makefile
 
+dist:
+
 pep8: # Check for PEP8 compliance
 	pep8 --first ${SRC}
+
+install: # Install
+	python3 distribute_setup.py check build install
 
 pylint: # Analyse Source
 	pylint -f html --files-output=y
--- doc/sample-invocation.sh
+++ doc/sample-invocation.sh
@@ -3,6 +3,8 @@
 #
 # List of sample commands to show how the scripts within pyPEBEL can be used.
 #
+# I am going to assume that you have installed the module.
+#
 
 ## --------------------------------------------------------------- [ Data File ]
 echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
@@ -16,34 +18,34 @@ culpa qui officia deserunt mollit anim id est laborum." > myfile.data
 ## ------------------------------------------------------------------ [ CP-ABE ]
 
 ## ------------------------------------------------------------------- [ Setup ]
-python pebel/scripts/pyCPABE-setup.py
+pyCPABE-setup.py
 
 ## ------------------------------------------------------------------ [ KeyGen ]
-python pebel/scripts/pyCPABE-keygen.py \
+pyCPABE-keygen.py \
     --mpk cp.mpk \
     --msk cp.msk \
     --dkey right.cpabe.dkey \
     one two three four
 
-python pebel/scripts/pyCPABE-keygen.py \
+pyCPABE-keygen.py \
     --mpk cp.mpk \
     --msk cp.msk \
     --dkey wrong.cpabe.dkey \
     five six seven eight
 
 ## ----------------------------------------------------------------- [ Encrypt ]
-python pebel/scripts/pyCPABE-encrypt.py \
+pyCPABE-encrypt.py \
     --mpk cp.mpk \
     --ptxt myfile.data \
     '(ONE and TWO) or THREE'
 
 ## ----------------------------------------------------------------- [ Decrypt ]
-python pebel/scripts/pyCPABE-decrypt.py \
+pyCPABE-decrypt.py \
     --mpk cp.mpk \
     --ctxt myfile.data.cpabe \
     --dkey right.cpabe.dkey
 
-python pebel/scripts/pyCPABE-decrypt.py \
+pyCPABE-decrypt.py \
     --mpk cp.mpk \
     --ctxt myfile.data.cpabe \
     --dkey wrong.cpabe.dkey
@@ -51,33 +53,33 @@ python pebel/scripts/pyCPABE-decrypt.py \
 ## ------------------------------------------------------------------ [ KP-ABE ]
 
 ## ------------------------------------------------------------------- [ Setup ]
-python pebel/scripts/pyKPABE-setup.py
+pyKPABE-setup.py
 ## ------------------------------------------------------------------ [ KeyGen ]
-python pebel/scripts/pyKPABE-keygen.py \
+pyKPABE-keygen.py \
     --mpk kp.mpk \
     --msk kp.msk \
     --dkey right.kpabe.dkey \
     '(one and two) or (three and four)'
 
-python pebel/scripts/pyKPABE-keygen.py \
+pyKPABE-keygen.py \
     --mpk kp.mpk \
     --msk kp.msk \
     --dkey wrong.kpabe.dkey \
     '(five and six) or (seven and eight)'
 
 ## ----------------------------------------------------------------- [ Encrypt ]
-python pebel/scripts/pyKPABE-encrypt.py \
+pyKPABE-encrypt.py \
     --mpk kp.mpk \
     --ptxt myfile.data \
     'one two five eight'
 
 ## ----------------------------------------------------------------- [ Decrypt ]
-python pebel/scripts/pyKPABE-decrypt.py \
+pyKPABE-decrypt.py \
     --mpk kp.mpk \
     --ctxt myfile.data.cpabe \
     --dkey right.kpabe.dkey
 
-python pebel/scripts/pyKPABE-decrypt.py \
+pyKPABE-decrypt.py \
     --mpk kp.mpk \
     --ctxt myfile.data.cpabe \
     --dkey wrong.kpabe.dkey
